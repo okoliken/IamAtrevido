ea975ac16e437e1aa3ee781063329cd563519e56
Okoli Jahbuchim <jahbuchimokoli3@gmail.com>
2023-05-08 17:45:06 +0100

    remove gitignore files

---
+++ src/lib/Socials.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import { Button } from "spaper";
+</script>
+
+<div class="row flex-right at-t4">
+  <Button
+    class="animate__animated animate__jello animate__delay-1s animate__repeat-2"
+    href="https://github.com/okoliken"
+    target="_blank"
+  >
+    Github
+  </Button>
+  <Button
+    class="animate__animated animate__swing animate__delay-1s animate__repeat-2"
+    href="https://twitter.com/jeff_okoli"
+    target="_blank"
+  >
+    Twitter
+  </Button>
+  <Button
+    class="animate__animated animate__tada animate__delay-1s animate__repeat-2"
+    href="https://www.linkedin.com/in/jahbuchim-okoli-0b1719224/"
+    target="_blank"
+  >
+    LinkedIn
+  </Button>
+</div>
+
+<style>
+  .at-t4 {
+    transform: translateX(-10px) translateY(40px);
+  }
+</style>
